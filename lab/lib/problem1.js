20bf27465d7664b00ddeb32424ff460f9d38041f
ta_theesaran <ta_theesaran@kkumail.com>
2021-08-25 16:57:36 +0700

    add lab5 problem1

---
+++ lab/lib/problem1.js
@@ -0,0 +1,27 @@
+var inputNumber, formatNumber;
+do {
+    inputNumber = prompt("Enter a postive integer", "");
+    formatNumber = Math.floor(Number(inputNumber));
+} while (!(formatNumber !== Infinity && String(formatNumber) === inputNumber && formatNumber > 0));
+showPrimes(parseInt(inputNumber));
+
+function showPrimes(nunber) {
+    if (nunber == 1) {
+        alert("For n = 1 don't have prime numbers");
+    } else {
+        let ansPrime = "For n = " + nunber + " prime numbers are ";
+        for (let i = 2; i <= nunber; i++) {
+            if (!isPrime(i)) continue;
+            console.log(i);
+            ansPrime += (i + ",");
+        }
+        alert(ansPrime.slice(0, -1));
+    }
+
+}
+function isPrime(nunber) {
+    for (let i = 2; i < nunber; i++) {
+        if (nunber % i == 0) return false;
+    }
+    return true;
+}
